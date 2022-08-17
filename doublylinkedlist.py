82ffd70ec25344ea1ba842724ec2952996113f6b
Meesum Abbas <79690513+meesum5445@users.noreply.github.com>
2022-08-17 16:08:43 +0500

    Update doublylinkedlist.py

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -22,13 +22,25 @@ class DLL:
             print("This DL list is empty")
         else:
             temp=self.head
-            for i in range(5):
+            for i in range(self.count()):
                 print(temp.data,"<-->",end="")
                 temp=temp.next
+    def count(self):
+          temp=self.head
+          len=0
+          while temp:
+            temp=temp.next
+            len=len+1
+          return len  
 l1=DLL()
 l1.append(5)
 l1.append(10)
 l1.append(15)
 l1.append(20)
 l1.append(25)
+l1.append(30)
+l1.append(35)
+l1.append(40)
+
 l1.display()
+print(l1.count())
